07fa549cc46fc72287d55d71d6d9f2f81e11e289
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-05-25 10:45:47 +0530

    Signing up using Database

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 export function GET(req: NextResponse) {
+
     return NextResponse.json({
         email: "mayank@gmail.com",
         name: "Mayank"
@@ -9,8 +13,21 @@ export function GET(req: NextResponse) {
 
 export async function POST(req: NextResponse) {
     const body = await req.json();
-
-    return NextResponse.json({
-        body
-    })
+    try{
+        await client.user.create({
+            data: {
+                email: body.email,
+                password: body.password
+            }
+        });
+        return NextResponse.json({
+            body
+        })
+    } catch(e) {
+        NextResponse.json({
+            message: "Error while Sign up",
+        }, {
+            status: 411
+        })
+    }
 }
